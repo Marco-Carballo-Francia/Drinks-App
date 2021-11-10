0bb442f7993137d9ee93f00d60f287dec622b9dd
David Heredia <davis_heredia@hotmail.com>
2021-11-10 05:05:12 -0300

    mejorasCSS

--- client/src/components/Profile/Addresses.jsx
+++ client/src/components/Profile/Addresses.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import style from './Styles/Addresses.module.css'
+import { BsPencilSquare } from "react-icons/bs";
 
 function Addresses() {
 
     return (
-        <div>
-            <p> Addresses</p>
+        <div className={style.data}>
+            <h3 className={style.titlePrin}> Mis direcciones <BsPencilSquare className={style.icon}/></h3>
+            
         </div>
     )
 }
