e80915482b8ccb866196759bda023555506aa2e2
Tpereletegui <tpereletegui@hotmail.com>
2021-11-10 00:15:41 -0300

    api

--- src/routes/ticket/ticket.routes.js
+++ src/routes/ticket/ticket.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { makePayment, getTicketsInPending, createTicket } = require("./ticket.controller.js")
+const { makePayment, getTicketsInPending, createTicket, getUserTickets } = require("./ticket.controller.js")
 
 const router = Router();
 
 router.post("/checkout", makePayment);
 router.get("/pending", getTicketsInPending);
 router.post("/create", createTicket);
+router.get("/user/:id",getUserTickets )
 
 module.exports =  router;
