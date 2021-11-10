7966809887a0b2463f4afea153792305fca64a58
David Heredia <davis_heredia@hotmail.com>
2021-11-10 02:46:29 -0300

    PutProfile

--- client/src/components/Profile/Dates.jsx
+++ client/src/components/Profile/Dates.jsx
@@ -1,31 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from "react-redux";
 import style from './Styles/Dates.module.css'
 
 function Dates() {
     const { user } = useSelector((state) => state.user);
+    const [state, setState] = useState(false)
+    const [errors, setErrors] = useState({});
 
-    // console.log(user)
+    const [values, setValues] = useState({
+        nombre: '',
+        apellido: ''
+    });
+
+    let validateLetras = /^[A-Z]+$/i
+    const validate = () => {
+        let errors = {};
+        if (!validateLetras.test(values.nombre)) {
+            errors.nombre = "debe ser solo letras";
+        }
+        if (!validateLetras.test(values.apellido)) {
+            errors.apellido = "debe ser solo letras";
+        }
+        return errors;
+    };
+
+    const handleOnChange = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value,
+        })
+        setErrors(
+            validate({
+                ...values,
+                [e.target.name]: e.target.value,
+            })
+        );
+    }
+
+    // const handleOnSumit = e => {
+    //     e.preventDefault();
+    //     if (
+    //         !errors.nombre &&
+    //         !errors.apellido &&
+    //     ) {
+    //         // dispatch(registerLocal(values));
+
+    //     } else {
+    //         alert("The form is required");
+    //     }
+    // }
+
+
     return (
-        <div className={style.ctn} >
+        <div>
+            {
+                state === false ? (
+                    <div className={style.ctn} >
+                        <div className={style.data}>
+                            <p className={style.title} >Nombre:</p>
+                            <p className={style.user}>{user.nombre ? user.nombre : user.user.nombre} </p>
+                        </div>
+                        <div className={style.data}>
+                            <p className={style.title} >Apellido:</p>
+                            <p className={style.user}>{user.apellido ? user.apellido : user.user.apellido} </p>
+                        </div>
+                        <div className={style.data}>
+                            <p className={style.title} >Email:</p>
+                            <p className={style.user}>{user.email ? user.email : user.user.email}</p>
+                        </div>
+                        <div className={style.data}>
+                            <p className={style.title} >Documento:</p>
+                            <p className={style.user}></p>
+                        </div>
+                        <div className={style.data}>
+                            <p className={style.title} >Telefono:</p>
+                            <p className={style.user}></p>
+                        </div>
+                    </div>
+                ) :
+                    <div className={style.Register}>
+                        <form className={style.form}  >
+                            <div className={style.username}>
+                                <label className={style.title}>Nombre</label>
+                                <input className={style.input}
+                                    name='nombre'
+                                    type="text"
+                                    placeholder='Su nombre...'
+                                    value={values.nombre}
+                                    onChange={handleOnChange}
+                                />
+                                <p className={style.error}>{errors.nombre}</p>
+                            </div>
 
-            <div className={style.data}>
-                <p className={style.title} >Nombre:</p>
-                <p className={style.user}>{user?.nombre}</p>
-            </div>
-            <div className={style.data}>
-                <p className={style.title} >Email:</p>
-                <p className={style.user}>{user?.email}</p>
-            </div>
-            <div className={style.data}>
-                <p className={style.title} >Documento:</p>
-                <p className={style.user}></p>
-            </div>
-            <div className={style.data}>
-                <p className={style.title} >Telefono:</p>
-                <p className={style.user}></p>
-            </div>
+
+                            <div className={style.email}>
+                                <label className={style.title}>Apellido</label>
+                                <input className={style.input}
+                                    name='apellido'
+                                    type="text"
+                                    placeholder='Su apellido...'
+                                    value={values.apellido}
+                                    onChange={handleOnChange}
+                                />
+                                <p className={style.error}>{errors.apellido}</p>
+                            </div>
+                        </form>
+                    </div>
+
+
+            }
+
         </div>
+
     )
 }
 
--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const passport = require("passport");
-const { postUser, postLogin, profileAuthenticate, googleLogin } = require("./user.controller.js");
+const { postUser, postLogin, profileAuthenticate, googleLogin, editUser } = require("./user.controller.js");
 // getUser, getUserById, getUserByNP otros contolers
 
 const router = Router();
@@ -11,5 +11,6 @@ router.post('/user/register', passport.authenticate("register", { session: false
 router.post('/user/login', postLogin);
 router.get('/user/profile', passport.authenticate('jwt', { session: false }), profileAuthenticate);
 router.post('/user/google', googleLogin)
+router.put('/user/edit/:id', editUser);
 
 module.exports = router;
