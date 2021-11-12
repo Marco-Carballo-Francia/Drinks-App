cda9d5423096dcbbdc00d060475fb6ddc5812743
Marco-Carballo-Francia <solojuegos.kiku2020@gmail.com>
2021-11-12 16:05:01 -0300

    Rutas

--- src/routes/ticket/ticket.controller.js
+++ src/routes/ticket/ticket.controller.js
@@ -61,7 +61,7 @@ const createTicket = async (req, res) => {
 const getTicketsInPendAndPro = async (req, res) => {
   try {
     let userTicketsPending = await Ticket.find({ state: "Pending" })
-      .populate("items", ["name", "precio"])
+      .populate("items.item", ["name", "precio"])
       .populate("user", ["nombre"]);
 
     let sortTicketsPending = userTicketsPending.sort((a, b) => {
@@ -104,12 +104,12 @@ const updateTickets = async (req, res) => {
   }
 };
 
-const getUserTickets = async (req, res) => {
+const getUserTicketsByID = async (req, res) => {
   const { id } = req.params;
   try {
     let userTickets = await Ticket.findById(id)
-      .populate("user")
-      .populate("items");
+      .populate("user", ["name"])
+      .populate("items.item");
     
     // function splitt(string) {
     //   let id = string.split('"');
