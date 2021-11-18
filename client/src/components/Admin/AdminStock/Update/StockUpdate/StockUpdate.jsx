512646fca2c189f79948b031c5acdcf24ec62166
Samuel <samuelcantos18@gmail.com>
2021-11-18 11:51:02 -0300

    cambiosParaAprobar

--- client/src/components/Admin/AdminStock/Update/StockUpdate/StockUpdate.jsx
+++ client/src/components/Admin/AdminStock/Update/StockUpdate/StockUpdate.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {deleteItem} from "../../../../../redux/actions/actions";
+import { deleteItem, getCategories, updateItem } from "../../../../../redux/actions/actions";
 import style from "./StockUpdate.module.css";
 
 const StockUpdate = () => {
@@ -8,19 +8,20 @@ const StockUpdate = () => {
 
     const [edit, setEdit] = useState(false);
     const { item } = useSelector(state => state.admin);
+    const { categories } = useSelector(state => state.products)
 
     const [object, setObject] = useState({
         nombre: "",
         precio: "",
         descripcion: "",
         imagen: "",
-        categorias: "",
+        categorias: [],
     })
 
     const handleDelete = () => {
         let answer = window.confirm(`Usted está por borrar el siguiente producto: ${item.nombre}`);
         if (answer) {
-            dispatch(deleteItem(item.id))
+            dispatch(deleteItem(item._id))
         }
         else {
             console.log("shkere")
@@ -36,47 +37,85 @@ const StockUpdate = () => {
         })
     }
 
+    const handleSelect = (e) => {
+        if (!object.categorias.includes(e.target.value)) {
+            setObject({
+                ...object,
+                categorias: [...object.categorias, e.target.value]
+            })
+        }
+    }
+
+
+
     const handleEdit = () => {
+        dispatch(getCategories())
         setObject({
             nombre: item.nombre,
             precio: item.precio,
             descripcion: item.descripcion,
             imagen: item.imagen,
-            categorias: "categoria",
+            categorias: item.categorias.map(el => el.nombre),
         })
         setEdit(true);
     }
 
     const handleSave = () => {
-        // acá va el dispatch que debería hacer un put en el item con la info actualizada
+        dispatch(updateItem({id: item._id, object}))
+        setObject({
+            nombre: "",
+            precio: "",
+            descripcion: "",
+            imagen: "",
+            categorias: [],
+        })
     }
 
     return (
         <div className={style.container}>
             {
-                 !edit 
+                !edit
                     ? <div>
                         <p>Nombre: {item.nombre}</p>
                         <p>Precio:{item.precio}</p>
                         <p>Descripcion{item.descripcion}</p>
                         <p>Imagen{item.imagen}</p>
+                        <div>
+                            {item.categorias?.map(e =>
+                                <div>{e.nombre}</div>
+                            )}
+                        </div>
                     </div>
-                    :  <div>
+                    : <div>
                         <input className={style.input} name="nombre" value={object.nombre} placeholder="Nombre..." onChange={handleChange} />
                         <input className={style.input} name="precio" value={object.precio} placeholder="Precio..." onChange={handleChange} />
                         <textarea className={style.textarea} name="descripcion" value={object.descripcion} placeholder="Descripcion..." onChange={handleChange} />
                         <input className={style.input} name="imagen" value={object.imagen} placeholder="Imagen..." onChange={handleChange} />
+                        <select onChange={handleSelect}>
+                            <option value="categorias">Categorias</option>
+                            {
+                                categories.map((e) => {
+                                    return <option>{e.nombre}</option>
+                                })
+                            }
+                        </select>
+                        <div>
+                            {object.categorias?.map(e =>
+                                <div>{e}</div>
+                            )}
+                        </div>
+
                     </div>
             }
-            <div  className={style.ctnBtnsModal}>
-                {   
+            <div className={style.ctnBtnsModal}>
+                {
                     !edit
                         ? <button className={style.btnModal} onClick={handleDelete}>ELIMINAR</button>
                         : <button className={style.btnModal} onClick={handleSave}>GUARDAR</button>
                 }
-                { 
-                    !edit 
-                        ? <button className={style.btnModal} disabled={!item} onClick={handleEdit}>EDITAR</button> 
+                {
+                    !edit
+                        ? <button className={style.btnModal} disabled={!item} onClick={handleEdit}>EDITAR</button>
                         : <button className={style.btnModal} onClick={() => setEdit(false)}>CANCELAR</button>
                 }
             </div>
